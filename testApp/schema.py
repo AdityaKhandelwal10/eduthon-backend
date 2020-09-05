f5a05220e8d4de87d3097fe3f518aecbcf987bf9
Aditya Khandelwal <akaditya05@gmail.com>
2020-09-05 17:44:07 +0530

    Mutation for User added

--- testApp/schema.py
+++ testApp/schema.py
@@ -5,6 +5,7 @@ from .models import User
 from graphene import relay
 from graphene_django.filter import DjangoFilterConnectionField 
 from graphene import Field
+import stream_chat
 
 class UserModel(DjangoObjectType):
     class Meta:
@@ -16,4 +17,32 @@ class Query(graphene.ObjectType):
     users = relay.Node.Field(UserModel)
     all_users = DjangoFilterConnectionField(UserModel)
 
-    
+
+#mutations here
+
+
+class CreateUser(graphene.Mutation):
+    user = graphene.Field(UserModel)
+
+    class Arguments:
+        username = graphene.String(required= True)
+        password = graphene.String(required= True)
+        email = graphene.String(required= True)
+        jwt = graphene.String(required= False)
+        
+    def mutate(self, info, username, password, email):
+
+        chat_client = stream_chat.StreamChat(api_key="whkwvq9maq97", api_secret="dgdh3ck6ff3x4n8wfnfdbwe2tva3acb4qvh4kc2ynmgw2yfqme9tv6fzses5gxdu")
+        token = chat_client.create_token(username)
+
+        user = User(username = username,
+        email = email, 
+        category = category, jwt = token
+        )
+        user.set_password(password)
+        user.save()
+
+        return CreateUser(user = user)  
+
+class Mutation(graphene.ObjectType):
+    create_user = CreateUser.Field()
