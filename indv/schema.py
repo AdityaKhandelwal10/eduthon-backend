32fe93c950cbf5974cd0baa189c8d8a39e9c5147
Aditya Khandelwal <akaditya05@gmail.com>
2020-09-06 02:02:45 +0530

    Progress update mutation added

--- indv/schema.py
+++ indv/schema.py
@@ -55,8 +55,42 @@ class ProgressModelMutation(DjangoModelFormMutation):
     class Meta:
         form_class = ProgressForm
 
+#progress mutations
+class AppendIndvTaskProg(graphene.Mutation):
+    indvtask = Field(IndvTaskType)
+
+    class Arguments :
+        #user  = graphene.ID(required = False)
+        task = graphene.ID(required = True) #global id of indv Task
+        prog = graphene.Int(required = True)
+    
+    def mutate(self, info, task, prog):
+        task_object = IndvTask.objects.get(task = task)
+        task_object.prog = prog
+        task_object.save()
+
+        return AppendIndvTaskProg(indvtask = task_object)
+
+
+class UpdateProgress(graphene.Mutation):
+    progress_group = Field(ProgressModelType)
+
+    class Arguments:
+        obj_primarykey = graphene.ID(required = True) 
+        progress = graphene.Int()
+
+    def mutate(info,self, obj_primarykey, progress):
+        progress_object = ProgressModel.objects.get(pk = obj_primarykey)
+        progress_object.progress = progress
+        progress_object.save()
+
+        return UpdateProgress(progress_group = progress_object)
+
 
 
 class Mutation(graphene.ObjectType):
     createIndvTask = IndvTaskMutation.Field()
     addProgress = ProgressModelMutation.Field()
+
+    appendIndvProg =AppendIndvTaskProg.Field()
+    updateProgress = UpdateProgress.Field()
